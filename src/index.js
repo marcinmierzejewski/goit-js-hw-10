416ac427d315070393b536b7641d4c9e9fe08e4b
Marcin Mierzejewski <mierzejewski2you@gmail.com>
2022-06-07 23:11:50 +0200

    added HTTP request

--- src/index.js
+++ src/index.js
@@ -1,3 +1,46 @@
 import './css/styles.css';
 
 const DEBOUNCE_DELAY = 300;
+
+const fetchCountryName = document.querySelector("#search-box");
+const countryList = document.querySelector(".country-list");
+const countryInfo = document.querySelector(".country-info");
+
+fetchCountryName.addEventListener("input", (e) => {
+  fetchCountries(e.currentTarget.value)
+    .then((countries) => {
+      console.log(countries)
+      renderCountriesList(countries)
+    })
+    .then(console.log("sprawdzamm"))
+    .catch((error) => {
+      console.log(error);
+      countryList.innerHTML = "";
+    });
+});
+
+function fetchCountries(name) {
+  return fetch(
+    `https://restcountries.com/v3.1/name/${name}?fields=name,capital,population,flags,languages`
+  ).then((response) => {
+    if (!response.ok) {
+      throw new Error(response.status);
+    }
+    return response.json();
+  });
+}
+
+function renderCountriesList(countries) {
+  const markup = countries
+    .map(({name, capital, population, flags, languages}) => {
+      return `<li>
+          <p><b>Name</b>: ${name.official}</p>
+          <p><b>Capitol</b>: ${capital}</p>
+          <p><b>Population</b>: ${population}</p>
+          <p><b>Flags</b>: ${flags.svg}</p>
+          <p><b>Languages</b>: ${Object.values(languages)}</p>
+        </li>`;
+    })
+    .join("");
+    countryList.innerHTML = markup;
+}
