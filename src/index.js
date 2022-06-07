3b3c4addf3b403b1e7127f9bb7fc54aa6143730c
Marcin Mierzejewski <mierzejewski2you@gmail.com>
2022-06-07 23:58:47 +0200

    added lodash library

--- src/index.js
+++ src/index.js
@@ -1,28 +1,38 @@
 import './css/styles.css';
 
-const DEBOUNCE_DELAY = 300;
+const _ = require('lodash');
 
-const fetchCountryName = document.querySelector("#search-box");
-const countryList = document.querySelector(".country-list");
-const countryInfo = document.querySelector(".country-info");
+const DEBOUNCE_DELAY = 3000;
+let filterFields = "name,capital,population,flags,languages";
 
-fetchCountryName.addEventListener("input", (e) => {
-  fetchCountries(e.currentTarget.value)
-    .then((countries) => {
-      console.log(countries)
-      renderCountriesList(countries)
-    })
-    .then(console.log("sprawdzamm"))
-    .catch((error) => {
-      console.log(error);
-      countryList.innerHTML = "";
-    });
-});
+const fetchCountryName = document.querySelector('#search-box');
+const countryList = document.querySelector('.country-list');
+const countryInfo = document.querySelector('.country-info');
+
+fetchCountryName.addEventListener(
+  'input',
+  _.debounce(e => {
+    fetchCountries(e.target.value)
+      .then(countries => {
+        console.log(countries);
+        renderCountriesList(countries);
+      })
+      .then(console.log('sprawdzammm'))
+      .catch(error => {
+        console.log(error);
+        countryList.innerHTML = '';
+      });
+  }, DEBOUNCE_DELAY)
+);
 
 function fetchCountries(name) {
+  const params = new URLSearchParams({
+    fields: filterFields,
+  });
+
   return fetch(
-    `https://restcountries.com/v3.1/name/${name}?fields=name,capital,population,flags,languages`
-  ).then((response) => {
+    `https://restcountries.com/v3.1/name/${name}?${params}`
+  ).then(response => {
     if (!response.ok) {
       throw new Error(response.status);
     }
@@ -32,7 +42,7 @@ function fetchCountries(name) {
 
 function renderCountriesList(countries) {
   const markup = countries
-    .map(({name, capital, population, flags, languages}) => {
+    .map(({ name, capital, population, flags, languages }) => {
       return `<li>
           <p><b>Name</b>: ${name.official}</p>
           <p><b>Capitol</b>: ${capital}</p>
@@ -41,6 +51,6 @@ function renderCountriesList(countries) {
           <p><b>Languages</b>: ${Object.values(languages)}</p>
         </li>`;
     })
-    .join("");
-    countryList.innerHTML = markup;
+    .join('');
+  countryList.innerHTML = markup;
 }
